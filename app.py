e7a6b22413d6dcaa61f6ed366e178efaee90a3e5
Sachin Kumar Rao <rao274563@gmail.com>
2026-06-20 21:29:48 +0530

    Implement PDF text extraction using PyPDF2

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from rag import extract_text_from_pdf
+
 
 st.set_page_config(
     page_title="RAG Over Bharat",
@@ -8,21 +10,32 @@ st.set_page_config(
 
 st.title("🇮🇳 RAG Over Bharat")
 
-st.markdown("""
-Ask questions about Indian Government Schemes and documents using Gemini-powered RAg
-""")
+# st.markdown("""
+# Ask questions about Indian Government Schemes and documents using Gemini-powered RAg
+# """)
 
 uploaded_file = st.file_uploader(
     "Uploaded a PDF",
     type=["pdf"]
 )
 
-user_question = st.text_input(
-    "Ask a question"
-)
+# user_question = st.text_input(
+#     "Ask a question"
+# )
+
+# if uploaded_file:
+#     st.success(f"Uploaded file: {uploaded_file.name}")
+
+# if user_question:
+#     st.write(f"Question: {user_question}")
 
 if uploaded_file:
-    st.success(f"Uploaded file: {uploaded_file.name}")
+    text = extract_text_from_pdf(uploaded_file)
 
-if user_question:
-    st.write(f"Question: {user_question}")    
+    st.success("PDF Processed successfully!")
+    st.subheader("Preview")
+    st.text_area(
+        "Extracted Text",
+        text[:3000],
+        height=300
+    )
