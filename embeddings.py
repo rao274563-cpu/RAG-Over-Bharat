84830b66c14f2b6a714945811d08b9a686574bc3
Sachin Kumar Rao <rao274563@gmail.com>
2026-06-21 03:41:01 +0530

    Debug embedding error

--- embeddings.py
+++ embeddings.py
@@ -1,7 +1,27 @@
+# # Step5: Embeddings
+# import os
+# from dotenv import load_dotenv
+# from google import genai
+
+# load_dotenv()
+
+# client = genai.Client(
+#     api_key=os.getenv("GEMINI_API_KEY")
+# )
+
+# def get_embedding(text):
+#     response = client.models.embed_content(
+#         model="gemini-embedding-001",
+#         contents = text
+#     )
+
+#     return response.embeddings[0].values
+
 # Step5: Embeddings
 import os
 from dotenv import load_dotenv
 from google import genai
+import streamlit as st
 
 load_dotenv()
 
@@ -10,9 +30,14 @@ client = genai.Client(
 )
 
 def get_embedding(text):
-    response = client.models.embed_content(
-        model="gemini-embedding-001",
-        contents = text
-    )
+    try:
+        response = client.models.embed_content(
+            model="gemini-embedding-001",
+            contents=text
+        )
 
-    return response.embeddings[0].values
+        return response.embeddings[0].values
+
+    except Exception as e:
+        st.error(f"Embedding Error: {repr(e)}")
+        raise
